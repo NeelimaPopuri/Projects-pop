42f5e5831a21dc7e7c0129257c3eb6ff1fc46044
NeelimaPopuri <popurineelima5@gmail.com>
2025-07-23 00:32:30 -0700

    pass

--- passwordd.py
+++ passwordd.py
@@ -3,13 +3,22 @@ from cryptography.fernet import Fernet
 master_pwd = input("What is the master password?")
 
 
-def write_key():
+'''def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)
+        key_file.write(key)'''
 
 
-write_key()
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+master_pwd = input("What is the master password?")
+key = load_key()
+fer = Fernet(key)
 
 
 def view():
@@ -17,7 +26,8 @@ def view():
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:", user, "| Password:", passw)
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
 
 
 def add():
@@ -25,7 +35,7 @@ def add():
     pwd = input("Password:")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
