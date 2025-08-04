b21347655c835249bf30528ad6047fa18aa1e09e
NeelimaPopuri <popurineelima5@gmail.com>
2025-08-03 21:43:21 -0700

    racers py

---
+++ race.py
@@ -0,0 +1,30 @@
+import turtle
+import tkinter as TK
+
+
+WIDTH, HEIGHT = 500, 500
+
+
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input("Enter the number of racers(2-10)")
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print("Input is invalid Number")
+            continue
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print("Numbers not in a range 2-10.. Try Again....")
+
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title("Turtle Racing")
+
+
+racers = get_number_of_racers()
+print(init_turtle)
