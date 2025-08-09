e825e8850adc5e14b9097280179b4f26ed0439cb
NeelimaPopuri <popurineelima5@gmail.com>
2025-08-08 22:04:33 -0700

    wppm

---
+++ wpm.py
@@ -0,0 +1,42 @@
+import curses
+from curses import wrapper
+
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the speed Typing Test.")
+    stdscr.addstr("\n Press any key to begin! ")
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+def wpm_test(stdscr):
+    target_text = "Hello World  this is some test text for this app"
+    current_text = []
+
+    while True:
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
+        current_text.append(key)
+
+        stdscr.clear()
+        stdscr.addstr(target_text)
+
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+
+        stdscr. getkey()
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    start_screen(stdscr)
+    wpm_test(stdscr)
+
+
+wrapper(main)
