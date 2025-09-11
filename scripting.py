4cce4c2e05c617d0664bf24f8160d6076ce5dff1
NeelimaPopuri <popurineelima5@gmail.com>
2025-09-11 12:00:52 -0700

    created new directory

--- scripting.py
+++ scripting.py
@@ -13,10 +13,29 @@ def find_all_game_paths(source):
     for root, dirs, files in os.walk(source):
         for directory in dirs:
             if GAME_DIR_PATTERN in directory.lower():
-                path = os.path.join(source, directory)
+                path = os.path.join(root, directory)
                 game_paths.append(path)
-        break
     return game_paths
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+def copy_and_overwrite(source, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(source, dest)
+
+
+def get_name_from_paths(paths, to_strip):
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_strip, "")
+        new_names.append(new_dir_name)
+    return new_names
 
 
 def main(source, target):
@@ -25,7 +44,13 @@ def main(source, target):
     target_path = os.path.join(cwd, target)
 
     game_paths = find_all_game_paths(source_path)
-    print(game_paths)
+    new_game_dirs = get_name_from_paths(game_paths, "game")
+
+    create_dir(target_path)
+
+    for src, dest in zip(game_paths, new_game_dirs):
+        dest_path = os.path.join(target_path, dest)
+        copy_and_overwrite(src, dest_path)
 
 
 if __name__ == "__main__":
