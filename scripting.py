1ed69465af5e098c885b88323512fb056e6de11e
NeelimaPopuri <popurineelima5@gmail.com>
2025-09-11 12:45:10 -0700

    change directory

--- scripting.py
+++ scripting.py
@@ -5,6 +5,8 @@ from subprocess import PIPE, run
 import sys
 
 GAME_DIR_PATTERN = "game"
+GAME_CODE_EXTENSION = ".go"
+GAME_COMPILE_COMMAND = ["go", "build"]
 
 
 def find_all_game_paths(source):
@@ -13,8 +15,9 @@ def find_all_game_paths(source):
     for root, dirs, files in os.walk(source):
         for directory in dirs:
             if GAME_DIR_PATTERN in directory.lower():
-                path = os.path.join(root, directory)
+                path = os.path.join(source, directory)
                 game_paths.append(path)
+        break
     return game_paths
 
 
@@ -47,6 +50,29 @@ def get_name_from_paths(paths, to_strip):
     return new_names
 
 
+def compile_game_code(path):
+    code_file_name = None
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(GAME_CODE_EXTENSION):
+                code_file_name = file
+                break
+        break
+    if code_file_name is None:
+        return
+    command = GAME_COMPILE_COMMAND + [code_file_name]
+    run_command(command, path)
+
+
+def run_command(command, path):
+    cwd = os.getcwd()
+    os.chdir(path)
+
+    result = run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    print("complie result", result)
+    os.chdir(cwd)
+
+
 def main(source, target):
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
@@ -60,9 +86,9 @@ def main(source, target):
     for src, dest in zip(game_paths, new_game_dirs):
         dest_path = os.path.join(target_path, dest)
         copy_and_overwrite(src, dest_path)
-
-    game_paths = find_all_game_paths(cwd, source)
-    new_game_dirs = get_name_from_paths(game_paths, "_game")
+        compile_game_code(dest_path)
+    json_path = os.path.join(target_path, "metadata.json")
+    make_json_metadata_file(json_path, new_game_dirs)
 
 
 if __name__ == "__main__":
